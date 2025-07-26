2c70220593d14a0b3d4678ec481d5d2568b566fd
Fanfan Rudi Afandi <vext@Fanfans-MacBook-Pro.local>
2025-07-26 23:08:55 +0700

    Fix routes

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,19 +2,22 @@ import { createClient } from '@/lib/supabase/server'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const { searchParams, origin } = new URL(request.url)
+  const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
-  // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/'
 
   if (code) {
     const supabase = createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`)
+      // Create a new URL object from the request URL
+      const url = new URL(request.url)
+      // Construct the final redirect URL using the correct host and the 'next' path
+      return NextResponse.redirect(`${url.origin}${next}`)
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+  const url = new URL(request.url)
+  return NextResponse.redirect(`${url.origin}/auth/auth-code-error`)
 }
